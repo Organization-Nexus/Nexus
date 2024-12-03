ef6f8c3b3456b399bbc14df0aa65b23d38752706
Jng-su <jeon95ujong@gmail.com>
2024-12-03 15:11:50 +0900

    ♻️ Refactor : backend CORS setting

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.BE_PORT;
+  const API_PREFIX = process.env.API_PREFIX;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -16,12 +17,17 @@ async function bootstrap() {
     }),
   );
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(API_PREFIX);
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(PORT, () => {
-    console.log(
-      `🚀 Running API in Mode ${process.env.NODE_ENV} on Port : ${PORT}`,
-    );
+    console.log(`🚀 Running API in Mode: ${process.env.NODE_ENV}`);
+    console.log(`📡 Backend URL: ${PORT}`);
   });
 }
 bootstrap();
