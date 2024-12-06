b40715f263d65c04c01ec2ce38ae157eca2979a9
Jng-su <jeon95ujong@gmail.com>
2024-12-06 16:14:47 +0900

    ⚙️ Setting : Custom Error Handling

---
+++ backend/src/common/dto/exception.response.dto.ts
@@ -0,0 +1,9 @@
+export class ResponseDto {
+  statusCode: number;
+  message: string;
+
+  constructor(statusCode: number, message: string) {
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
