22b30bcd0fa1591125eb696c6033deaf15744a46
kyung <bkykim12@gmail.com>
2024-12-30 18:18:12 +0900

    🎯 Fix : middleware.ts config matcher 수정

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -23,5 +23,5 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|images|favicon.ico).*)"],
 };
