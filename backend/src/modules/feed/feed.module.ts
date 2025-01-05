b9b303385cf92eef5b2b19b92b38ad0b0259598f
Jng-su <jeon95ujong@gmail.com>
2025-01-05 19:35:23 +0900

    🚧 Progress : File upload to s3 - detail path, Feed Entity - Create ...ing

--- backend/src/modules/feed/feed.module.ts
+++ backend/src/modules/feed/feed.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { FeedService } from './feed.service';
+import { FeedController } from './feed.controller';
 import { FileModule } from '../file/file.module';
 import { Feed } from './entites/feed.entity';
 import { ProjectUser } from '../project-user/entites/project-user.entity';
@@ -8,7 +9,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Feed, ProjectUser, Project]), FileModule],
-  controllers: [],
+  controllers: [FeedController],
   providers: [FeedService],
 })
 export class FeedModule {}
