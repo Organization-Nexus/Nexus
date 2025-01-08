e3c00f9495de78098e6e39e4878f5baf44404816
kyung <bkykim12@gmail.com>
2025-01-08 16:25:56 +0900

    ✨ Feat : 비밀번호 재설정 SMTP 구현

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,6 +13,9 @@ import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { join } from 'path';
 import { FeedModule } from './modules/feed/feed.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -29,6 +32,28 @@ import { FeedModule } from './modules/feed/feed.module';
     FileModule,
     ProjectUserModule,
     FeedModule,
+
+    MailerModule.forRoot({
+      transport: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        secure: false,
+        auth: {
+          user: process.env.GMAIL_USER,
+          pass: process.env.GMAIL_APP_PASSWORD,
+        },
+      },
+      defaults: {
+        from: '"No Reply" <nexus_official@gmail.com>',
+      },
+      template: {
+        dir: join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
 })
 export class AppModule {
