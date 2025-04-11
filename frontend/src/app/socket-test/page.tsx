601246975b046a7aa3c012c7cb5fa4e523ec120e
kyung <bkykim12@gmail.com>
2025-04-11 15:43:15 +0900

    🔥 Remove : socket-test page.tsx 주석처리

--- frontend/src/app/socket-test/page.tsx
+++ frontend/src/app/socket-test/page.tsx
@@ -1,12 +1,12 @@
-// app/socket-test/page.tsx
-import SimpleSocketTest from "@/components/utils/SimpleSocketTest";
+// // app/socket-test/page.tsx
+// import SimpleSocketTest from "@/components/utils/SimpleSocketTest";
 
-export default function SocketTestPage() {
-  return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-6">웹소켓 테스트 페이지</h1>
+// export default function SocketTestPage() {
+//   return (
+//     <div className="container mx-auto p-6">
+//       <h1 className="text-2xl font-bold mb-6">웹소켓 테스트 페이지</h1>
 
-      <SimpleSocketTest />
-    </div>
-  );
-}
+//       <SimpleSocketTest />
+//     </div>
+//   );
+// }
