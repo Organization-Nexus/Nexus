2e4261c049e2dd4b8f69965177962eacb7649a6e
kyung <bkykim12@gmail.com>
2025-04-14 18:31:57 +0900

    💄 Style : dashboard UI

--- frontend/src/app/myproject/[projectId]/dashboard/page.tsx
+++ frontend/src/app/myproject/[projectId]/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import { projectApi } from "@/app/_api/models/project";
-import CardSection from "@/components/dashboard/CardSection";
+import CommunityCardSection from "@/components/dashboard/CommunityCardSection";
+import MinutesCardSection from "@/components/dashboard/MinutesCardSection";
 import ProjectInfo from "@/components/dashboard/ProjectInfo";
 import ProjectUsers from "@/components/dashboard/ProjectUsers";
+import TodoCardSection from "@/components/dashboard/TodoCardSection";
 import { ProjectIdProps } from "@/types/project";
 
 export default async function Dashboard({ params }: ProjectIdProps) {
@@ -26,25 +28,17 @@ export default async function Dashboard({ params }: ProjectIdProps) {
         <div className="grid grid-cols-5 grid-rows-2 gap-4 ">
           {/* 왼쪽 Column */}
           <div className="row-span-2 col-span-2">
-            <CardSection title="Creeper 커뮤니티">
-              <div className="space-y-2">
-                {Array(6)
-                  .fill(null)
-                  .map((_, i) => (
-                    <div key={i} className="flex items-center gap-2">
-                      <div className="w-2 h-2 bg-green-400 rounded-full"></div>
-                      <span className="text-sm">제목입니다.</span>
-                    </div>
-                  ))}
-              </div>
-            </CardSection>
+            <CommunityCardSection
+              projectId={projectId}
+              title="Creeper 커뮤니티"
+            />
           </div>
 
           {/* 오른쪽 Column */}
           <div className="space-y-4 col-span-3">
-            <CardSection title="내가 담당중인 업무">02</CardSection>
+            <TodoCardSection projectId={projectId} title="내가 담당중인 업무" />
 
-            <CardSection title="일정">03</CardSection>
+            <MinutesCardSection projectId={projectId} title="회의록" />
           </div>
         </div>
       </div>
